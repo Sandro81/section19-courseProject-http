e91ddca680a09160529f3b8a96bdad7d62bedad8
Sandro81 <sandro.fioravanti81@gmail.com>
2019-09-25 21:03:44 +0100

    section 19

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,6 +1,20 @@
 import {Injectable} from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import {RecipeService} from '../recipes/recipe.service';
 
 @Injectable({providedIn: 'root'})
 export class DataStorageService {
-  constructor()
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+    return this.http.
+    put('https://section19-e6779.firebaseio.com/recipes.json',
+      recipes)
+      .subscribe(response => {
+        console.log(response);
+      });
+  }
+
 }
